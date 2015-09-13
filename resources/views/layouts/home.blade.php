d916024bc7d426e1d188195c0d143978cc00825d
Paulo Mogollon <wanchopeblanco@gmail.com>
2015-09-12 19:15:13 -0500

    Revert admin zopim and analytics

--- resources/views/layouts/home.blade.php
+++ resources/views/layouts/home.blade.php
@@ -195,20 +195,18 @@
         </script>
 
         @if(env('APP_ENV') == 'production')
-            @if( !(Auth::check() && Auth::user()->isAdmin()) )
-                <!--Start of Zopim Live Chat Script-->
-                <script type="text/javascript">
-                window.$zopim||(function(d,s){var z=$zopim=function(c){z._.push(c)},$=z.s=
-                d.createElement(s),e=d.getElementsByTagName(s)[0];z.set=function(o){z.set.
-                _.push(o)};z._=[];z.set._=[];$.async=!0;$.setAttribute("charset","utf-8");
-                $.src="//v2.zopim.com/?3JSQBAV4Z1Lfbp8037PLzpSnUFdRn1EV";z.t=+new Date;$.
-                type="text/javascript";e.parentNode.insertBefore($,e)})(document,"script");
-                </script>
-                <!--End of Zopim Live Chat Script-->
-                
-                <!-- Other Scripts -->
-                {!! Analytics::render() !!}
-            @endif
+            <!--Start of Zopim Live Chat Script-->
+            <script type="text/javascript">
+            window.$zopim||(function(d,s){var z=$zopim=function(c){z._.push(c)},$=z.s=
+            d.createElement(s),e=d.getElementsByTagName(s)[0];z.set=function(o){z.set.
+            _.push(o)};z._=[];z.set._=[];$.async=!0;$.setAttribute("charset","utf-8");
+            $.src="//v2.zopim.com/?3JSQBAV4Z1Lfbp8037PLzpSnUFdRn1EV";z.t=+new Date;$.
+            type="text/javascript";e.parentNode.insertBefore($,e)})(document,"script");
+            </script>
+            <!--End of Zopim Live Chat Script-->
+            
+            <!-- Other Scripts -->
+            {!! Analytics::render() !!}
         @endif
     </body>
 </html>
