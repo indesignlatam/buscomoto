875fed6cf243b41161329ae687ef0c533be00824
Paulo Mogollon <wanchopeblanco@gmail.com>
2015-09-16 07:26:15 -0500

    User listings enhancements #35

--- resources/views/user/show.blade.php
+++ resources/views/user/show.blade.php
@@ -16,10 +16,11 @@
 @section('content')
 
 <div class="uk-cover-background uk-position-relative">
-    <img class="" src="{{ asset(Image::url('/images/defaults/user_front.jpg',['full_page'])) }}" width="100%" alt="">
-    <div class="uk-position-cover uk-flex uk-flex-center uk-flex-middle uk-visible-small">
-        <h1 class="uk-text-contrast uk-text-bold">{{ strtoupper($user->name) }}</h1>
-    </div>
+	@if($user->image_path)
+    	<img class="" src="{{ asset($user->image_path) }}" width="100%" alt="">
+	@else
+    	<img class="" src="{{ asset('/images/defaults/user_front.jpg') }}" width="100%" alt="">
+	@endif
     <div class="uk-position-cover uk-flex uk-flex-center uk-flex-middle uk-hidden-small">
         <h1 class="uk-text-contrast uk-text-bold" style="font-size:60px; margin-left:-30%;">{{ strtoupper($user->name) }}</h1>
     </div>
@@ -27,20 +28,37 @@
 
 <div class="uk-container uk-container-center uk-margin-top">
 	<div class="uk-panel uk-margin-bottom">
+		<div class="uk-visible-small">
+			<img src="{{ asset('favicon.ico') }}" style="max-width: 30%; margin-top: -60px" class="uk-align-center">
+			<h2 class="uk-text-primary uk-text-bold uk-margin-top-remove uk-text-center">{{ strtoupper($user->name) }}</h2>
+		</div>
+		
 		<h3 id="description">{{ $user->description }}</h3>
 
 		<!-- user listings -->
         @if(count($user->listings) > 0)
-            <h1 class="uk-text-bold">{{ trans('frontend.user_listings') }}</h1>
+            <h1 class="uk-text-bold uk-hidden-small">{{ trans('frontend.user_listings') }}</h1>
+            <hr class="uk-visible-small">
             <div class="uk-grid">
                 @foreach($user->listings as $listing)
                     <div class="uk-width-large-1-4 uk-width-medium-1-3 uk-width-small-1-1">
                         <a href="{{ url($listing->path()) }}">
-                            <img src="{{ asset(Image::url($listing->image_path(),['mini_front'])) }}" class="uk-margin-small-bottom" style="max-width=150px">
+                            <img src="{{ asset(Image::url($listing->image_path(),['mini_front_2x'])) }}" class="uk-margin-small-bottom">
                         </a>
                         <br class="uk-visible-small">
-                        <a href="{{ url($listing->path()) }}">{{ $listing->title }}</a>
-                        <p class="uk-text-muted" style="font-size:10px;margin-top:-4px">{{ money_format('$%!.0i', $listing->price) }} | {{ $listing->odometer }} kms</p>
+
+                        <div class="uk-visible-small">
+                        	<a href="{{ url($listing->path()) }}" class="uk-h4">{{ $listing->title }}</a>
+                        	<h4 class="uk-text-muted" style="margin-top:0px; margin-bottom:0px">
+                        		{{ money_format('$%!.0i', $listing->price) }} | {{ $listing->odometer }} kms
+                        	</h4>
+                        </div>
+
+                        <div class="uk-hidden-small">
+                        	<a href="{{ url($listing->path()) }}">{{ $listing->title }}</a>
+                        	<p class="uk-text-muted" style="font-size:10px;margin-top:-4px">{{ money_format('$%!.0i', $listing->price) }} | {{ $listing->odometer }} kms</p>
+                        </div>
+                        
                         <hr class="uk-visible-small uk-margin-bottom">
                     </div>
                 @endforeach
