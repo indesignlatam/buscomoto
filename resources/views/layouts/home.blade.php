200e70da7b5ff9d188fc2598acbdeae8ee1ae43a
Paulo Mogollon <wanchopeblanco@gmail.com>
2015-09-18 16:54:03 -0500

    No zoom everywhere

--- resources/views/layouts/home.blade.php
+++ resources/views/layouts/home.blade.php
@@ -3,7 +3,9 @@
     <head>
         <meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <!-- No zoom -->
+        <meta name="viewport" content="width=device-width, user-scalable=no"/>
+        <!-- No zoom -->
         <meta name="author" content="BuscoMoto.co">
         @section('head')
             <title>{{ Settings::get('site_name') }}</title>
