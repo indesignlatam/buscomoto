95b0a80777af44722409b323833dfae72ab5b3e2
Paulo Mogollon <wanchopeblanco@gmail.com>
2015-09-18 16:17:29 -0500

    Removed unnecessary IF

--- resources/views/listings/mobile/mosaic.blade.php
+++ resources/views/listings/mobile/mosaic.blade.php
@@ -1,21 +1,19 @@
-@if(isset($listing))
-	<div class="uk-width-medium-1-2 uk-width-large-1-2 uk-margin-small-bottom" id="listing_{{ $listing->id }}">		
-		<div class="uk-panel uk-panel-box uk-margin-remove">
-			<a href="{{ url($listing->path()) }}" style="text-decoration:none">
-				<img src="{{ asset(Image::url($listing->image_path(),['mini_image_2x'])) }}">
-	    		<div class="">
-	    			<p class="uk-margin-bottom-remove">
-	    				<strong class="uk-text-primary">{{ $listing->title }} 
-	    				@if($listing->featuredType && $listing->featured_expires_at > Carbon::now())
-						<i class="uk-text-success uk-icon-check"></i>
-						@endif
-						</strong>
-	    				<br>
-	    				<b class="uk-text-bold">{{ money_format('$%!.0i', $listing->price) }}</b> | 
-	    				<i class="uk-text-muted">{{ number_format($listing->odometer) }} kms</i>
-	    			</p>
-	    		</div>
-			</a>
-		</div>
+<div class="uk-width-medium-1-2 uk-width-large-1-2 uk-margin-small-bottom" id="listing_{{ $listing->id }}">		
+	<div class="uk-panel uk-panel-box uk-margin-remove">
+		<a href="{{ url($listing->path()) }}" style="text-decoration:none">
+			<img src="{{ asset(Image::url($listing->image_path(),['mini_image_2x'])) }}">
+    		<div class="">
+    			<p class="uk-margin-bottom-remove">
+    				<strong class="uk-text-primary">{{ $listing->title }} 
+    				@if($listing->featuredType && $listing->featured_expires_at > Carbon::now())
+					<i class="uk-text-success uk-icon-check"></i>
+					@endif
+					</strong>
+    				<br>
+    				<b class="uk-text-bold">{{ money_format('$%!.0i', $listing->price) }}</b> | 
+    				<i class="uk-text-muted">{{ number_format($listing->odometer) }} kms</i>
+    			</p>
+    		</div>
+		</a>
 	</div>
-@endif
+</div>
