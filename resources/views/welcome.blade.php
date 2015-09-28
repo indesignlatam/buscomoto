a70c331f1206dc3d6c6b6b3be52565889bacfdee
Paulo Mogollon <wanchopeblanco@gmail.com>
2015-09-27 19:26:55 -0500

    Removed pagination from welcome slider

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -355,9 +355,6 @@
                 direction: 'horizontal',
                 loop: true,
                 
-                // If we need pagination
-                pagination: '.swiper-pagination',
-                
                 // Navigation arrows
                 nextButton: '.swiper-button-next',
                 prevButton: '.swiper-button-prev',
