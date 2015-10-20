e59947bdef0754bec6edbd2034f52e12eb70f9be
Paulo Mogollon <wanchopeblanco@gmail.com>
2015-10-19 20:01:05 -0500

    TRY FIX

--- app/Http/Controllers/APIV2/ListingController.php
+++ app/Http/Controllers/APIV2/ListingController.php
@@ -431,7 +431,7 @@ class ListingController extends Controller {
 	 */
 	public function userListings(){
 		$user 	= User::find(Authorizer::getResourceOwnerId());
-		$listings 	= Listing::where('user_id', $user->id)->with('user', 'listingType', 'city', 'images', 'manufacturer', 'model', 'features')->get();
+		$listings 	= null;
 
 		return response()->json(['data' => $listings,
 								]);
